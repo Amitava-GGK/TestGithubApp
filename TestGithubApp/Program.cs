9246d81169b5eda9a74d5b7249af2fc1c348a60a
Amitava Karan <amitava.karan@ggktech.com>
2017-06-29 16:58:24 +0530

    TGA-1: Print hello world on console.

--- TestGithubApp/Program.cs
+++ TestGithubApp/Program.cs
@@ -10,6 +10,8 @@ namespace TestGithubApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey(true);
         }
     }
 }
